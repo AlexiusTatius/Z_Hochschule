02a4eb5a18b59a6c9886485ac6350db47ee00c5f
Mehtab Singh <mehtabsinghcse@gmail.com>
2024-04-22 04:22:54 +0530

    changed  multer middleware for dynamic path in multer-auth branch

---
+++ Backend/Commit_History.md
@@ -0,0 +1,2 @@
+1. The first commit includes uploading system of files.
+2. Created a seperate branch, includes changes code of multer.middleware.js file (dynamic file storage path).      Branch name: multer-auth
--- Backend/middlewares/multer.middlware.js
+++ Backend/middlewares/multer.middlware.js
@@ -3,10 +3,29 @@ import multer from 'multer';
 
 const maxSize = 2 * 1024 * 1024;
 
+const setDestination = async (req, file, cb) => {
+  try {
+    // Get the username of the logged-in user (e.g., 'Alice')
+    const loggedInUsername = req.user.username; // Assuming username is stored in req.user.username
+
+    // Find the user document corresponding to the logged-in username
+    const user = await User.findOne({ name: loggedInUsername });
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    // Use the folderPath from the user document as the destination path
+    const destinationPath = __basedir + '/uploads/' + user.folderPath + '/';
+
+    cb(null, destinationPath);
+  } catch (error) {
+    cb(error);
+  }
+};
+
 let storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, __basedir + "/uploads/");
-  },
+  destination: setDestination,
   filename: (req, file, cb) => {
     console.log(file.originalname);
     cb(null, file.originalname);
